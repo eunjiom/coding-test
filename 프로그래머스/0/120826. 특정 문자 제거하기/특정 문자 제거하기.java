c359616a1e331acd952c7dabb074cd05fee9f0c6
eunjiom <norgg021@gmail.com>
2026-07-29 22:14:21 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        // replace = (1,2) 1을 2로 바꿔줌
+        String answer = my_string.replace(letter,"");
+        return answer;
+    }
+}
+
+
+    
+    
