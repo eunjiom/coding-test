d214d194439bffb10668804862574c9f696c92be
eunjiom <norgg021@gmail.com>
2026-08-24 19:52:49 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 104.76 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        // 두 문자열 그냥 이어붙여서 출력
+        System.out.println(a + b);
+    }
+}
